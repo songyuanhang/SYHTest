c96166fbb989e32f897f4b8c15e1321faa1e4766
songyuanhang <752478559@qq.com>
2015-06-23 09:47:40 +0800

    SYH

---
+++ LZHText/LZHText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZHText
+//
+//  Created by lanou3g on 15/6/16.
+//  Copyright (c) 2015年 宋功德. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
